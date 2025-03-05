331304dbf6955eb8424dea0bad612e2d810d8dd3
PraNAtaP <pranatapu08@gmail.com>
2025-03-05 22:49:56 +0700

    Commit - Praktikum 2

--- MahasiswaDemoModif.java
+++ MahasiswaDemoModif.java
@@ -24,10 +24,10 @@ public class MahasiswaDemoModif {
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.println("NIM" + arrayOfMahasiswa[i].nim);
-            System.out.println("Nama" + arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas" + arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK" + arrayOfMahasiswa[i].ipk);
+            System.out.println("NIM     : " + arrayOfMahasiswa[i].nim);
+            System.out.println("Nama    : " + arrayOfMahasiswa[i].nama);
+            System.out.println("Kelas   : " + arrayOfMahasiswa[i].kelas);
+            System.out.println("IPK     : " + arrayOfMahasiswa[i].ipk);
             System.out.println("-------------------------");
         }
 
